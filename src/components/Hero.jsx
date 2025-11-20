644242c766748a3af476e8c76ae5f932fb1cc309
manav-05-06 <manavkhandelwal72@gmail.com>
2025-11-20 15:24:21 +0530

    Updated entire project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -109,7 +109,7 @@ function Hero() {
         {/* ⭐ Resume Button */}
         <motion.a
           whileHover={{ scale: 1.07 }}
-          href="/Manav_khandelwal.pdf"
+          href="/Manav_Khandelwal.pdf"
           target="_blank"
           className="
             px-7 py-3 rounded-xl font-semibold 
